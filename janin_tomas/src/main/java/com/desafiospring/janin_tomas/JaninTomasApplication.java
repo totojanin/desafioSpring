a3ff79edc606bb7616ced2ffaff085cedf157721
totojanin <tomas.janin@mercadolibre.com>
2021-04-06 11:01:58 -0300

    primer commit

---
+++ janin_tomas/src/main/java/com/desafiospring/janin_tomas/JaninTomasApplication.java
@@ -0,0 +1,13 @@
+package com.desafiospring.janin_tomas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaninTomasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaninTomasApplication.class, args);
+	}
+
+}
---
+++ janin_tomas/src/test/java/com/desafiospring/janin_tomas/JaninTomasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiospring.janin_tomas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaninTomasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
